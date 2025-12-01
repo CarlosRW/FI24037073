752974b24578676bef6123e96abb7491bbdb42be
CarlosRW <carlos.ramirezw23@gmail.com>
2025-12-01 00:42:57 -0600

    Start of the CP2

---
+++ CP2/Main/src/main/java/com/carlosrw/casopractico2/QueueNode.java
@@ -0,0 +1,28 @@
+package com.carlosrw.casopractico2;
+
+/**
+ *
+ * @author carlos
+ */
+public class QueueNode {
+
+    private int _index;
+    private QueueNode _next;
+
+    public QueueNode(int index) {
+        _index = index;
+        _next = null;
+    }
+
+    public int getIndex() {
+        return _index;
+    }
+
+    public QueueNode getNext() {
+        return _next;
+    }
+
+    public void setNext(QueueNode next) {
+        _next = next;
+    }
+}
---
+++ CP2/Main/src/main/java/com/carlosrw/casopractico2/StackNode.java
@@ -0,0 +1,28 @@
+package com.carlosrw.casopractico2;
+
+/**
+ *
+ * @author carlos
+ */
+public class StackNode {
+
+    private String _word;
+    private StackNode _next;
+
+    public StackNode(String word) {
+        _word = word;
+        _next = null;
+    }
+
+    public String getWord() {
+        return _word;
+    }
+
+    public StackNode getNext() {
+        return _next;
+    }
+
+    public void setNext(StackNode next) {
+        _next = next;
+    }
+}
